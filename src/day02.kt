f42e7bdcc561bfbe69b995194b93d7112a7e4a69
Tiago Silva <sapuglha@gmail.com>
2022-12-02 08:48:43 -0700

    Day 2 - beautified

--- src/day02.kt
+++ src/day02.kt
@@ -1,126 +1,86 @@
+import java.lang.IllegalArgumentException
+
 fun main() {
-    val fileName = "day02"
+    fun part1(input: List<String>): Int = input.sumOf { line ->
+        val round = line.split(" ")
+        val theirMove = round.first().toHand()
+        val myMove = round.last().toHand()
 
-    fun part1(input: List<String>): Int {
-        var sumPoints = 0
-
-        input.forEach { line ->
-            val splitList = line.split(" ")
-            val hisHand = splitList.first()
-            val myHand = splitList.last()
-
-            when (hisHand) {
-                "A" /*rock*/ -> when (myHand) {
-                    "Y" /*paper - score 2*/-> {
-                        // win - score 6
-                        sumPoints += 8
-                    }
-                    "X" /*rock - score 1*/ -> {
-                        // draw - score 3
-                        sumPoints += 4
-                    }
-                    "Z" /*scissor - score 3*/ -> {
-                        // loss - score 0
-                        sumPoints += 3
-                    }
-                }
-
-                "B" /*paper*/ -> when (myHand) {
-                    "Y" /*paper - score 2*/-> {
-                        // draw - score 3
-                        sumPoints += 5
-                    }
-                    "X" /*rock - score 1*/ -> {
-                        // loss - score 0
-                        sumPoints += 1
-                    }
-                    "Z" /*scissor - score 3*/ -> {
-                        // win - score 6
-                        sumPoints += 9
-                    }
-                }
-
-                "C" /*scissors*/ -> when (myHand) {
-                    "Y" /*paper - score 2*/-> {
-                        // loss - score 0
-                        sumPoints += 2
-                    }
-                    "X" /*rock - score 1*/ -> {
-                        // win - score 6
-                        sumPoints += 7
-                    }
-                    "Z" /*scissor - score 3*/ -> {
-                        // tie - score 3
-                        sumPoints += 6
-                    }
-                }
+        when (theirMove) {
+            Hand.ROCK -> when (myMove) {
+                Hand.ROCK -> Outcome.DRAW.score + myMove.score
+                Hand.PAPER -> Outcome.WIN.score + myMove.score
+                Hand.SCISSOR -> Outcome.LOSE.score + myMove.score
             }
 
-        }
-        return sumPoints
-    }
-
-    fun part2(input: List<String>): Int {
-        var sumPoints = 0
-
-        input.forEach { line ->
-            val splitList = line.split(" ")
-            val hisHand = splitList.first()
-            val myHand = splitList.last()
-
-            when (hisHand) {
-                "A" /*rock*/ -> when (myHand) {
-                    "Y" /* draw- score 3 */ -> {
-                        //  rock - score 1
-                        sumPoints += 4
-                    }
-                    "X" /* lose - score 0 */ -> {
-                        //  scissor - score 3
-                        sumPoints += 3
-                    }
-                    "Z" /* win - score 6 */ -> {
-                        //  paper - score 2
-                        sumPoints += 8
-                    }
-                }
-
-                "B" /*paper*/ -> when (myHand) {
-                    "Y" /* draw- score 3 */ -> {
-                        // paper - score 2
-                        sumPoints += 5
-                    }
-                    "X" /* lose - score 0 */ -> {
-                        // rock - score 1
-                        sumPoints += 1
-                    }
-                    "Z" /* win - score 6 */ -> {
-                        // scissor - score 3
-                        sumPoints += 9
-                    }
-                }
-
-                "C" /*scissors*/ -> when (myHand) {
-                    "Y" /* draw- score 3 */ -> {
-                        // scissor - score 3
-                        sumPoints += 6
-                    }
-                    "X" /* lose - score 0 */ -> {
-                        // paper - score 2
-                        sumPoints += 2
-                    }
-                    "Z" /* win - score 6 */ -> {
-                        // rock - score 1
-                        sumPoints += 7
-                    }
-                }
+            Hand.PAPER -> when (myMove) {
+                Hand.ROCK -> Outcome.LOSE.score + myMove.score
+                Hand.PAPER -> Outcome.DRAW.score + myMove.score
+                Hand.SCISSOR -> Outcome.WIN.score + myMove.score
             }
 
+            Hand.SCISSOR -> when (myMove) {
+                Hand.ROCK -> Outcome.WIN.score + myMove.score
+                Hand.PAPER -> Outcome.LOSE.score + myMove.score
+                Hand.SCISSOR -> Outcome.DRAW.score + myMove.score
+            }
         }
-        return sumPoints
     }
 
-    val input = fileName.readFileAsLines()
+    fun part2(input: List<String>): Int = input.sumOf { line ->
+        val round = line.split(" ")
+        val theirMove = round.first().toHand()
+        val desiredOutcome = round.last().toOutcome()
 
-    println("part1: ${part1(input)}")
-    println("part2: ${part2(input)}")
+        when (theirMove) {
+            Hand.ROCK -> when (desiredOutcome) {
+                Outcome.WIN -> desiredOutcome.score + Hand.PAPER.score
+                Outcome.DRAW -> desiredOutcome.score + Hand.ROCK.score
+                Outcome.LOSE -> desiredOutcome.score + Hand.SCISSOR.score
+            }
+
+            Hand.PAPER -> when (desiredOutcome) {
+                Outcome.WIN -> desiredOutcome.score + Hand.SCISSOR.score
+                Outcome.DRAW -> desiredOutcome.score + Hand.PAPER.score
+                Outcome.LOSE -> desiredOutcome.score + Hand.ROCK.score
+            }
+
+            Hand.SCISSOR -> when (desiredOutcome) {
+                Outcome.WIN -> desiredOutcome.score + Hand.ROCK.score
+                Outcome.DRAW -> desiredOutcome.score + Hand.SCISSOR.score
+                Outcome.LOSE -> desiredOutcome.score + Hand.PAPER.score
+            }
+        }
+    }
+
+    "day02".readFileAsLines().let { input ->
+        println("part1: ${part1(input)}")
+        println("part2: ${part2(input)}")
+    }
+}
+
+enum class Hand(val score: Int) {
+    ROCK(1),
+    PAPER(2),
+    SCISSOR(3),
+}
+
+enum class Outcome(val score: Int) {
+    WIN(6),
+    DRAW(3),
+    LOSE(0),
+}
+
+fun String.toHand(): Hand = when (this) {
+    "A", "X" -> Hand.ROCK
+    "B", "Y" -> Hand.PAPER
+    "C", "Z" -> Hand.SCISSOR
+    else -> throw IllegalArgumentException()
+}
+
+fun String.toOutcome(): Outcome = when (this) {
+    "X" -> Outcome.LOSE
+    "Y" -> Outcome.DRAW
+    "Z" -> Outcome.WIN
+    else -> throw IllegalArgumentException()
 }
