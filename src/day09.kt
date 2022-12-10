639745a9231442e52fa92270626a02e7542eaa45
Tiago Silva <sapuglha@gmail.com>
2022-12-09 20:17:04 -0700

    Day 9 - cleanup

--- src/day09.kt
+++ src/day09.kt
@@ -1,32 +1,19 @@
 data class KnotPosition(
     val name: String,
-    var currentH: Int = 0,
-    var currentV: Int = 0,
+    var x: Int = 0,
+    var y: Int = 0,
 )
 
-fun KnotPosition.move(direction: String) =
-    when (direction) {
-        "U" -> {
-            currentV++
-        }
+fun KnotPosition.move(direction: String) = when (direction) {
+    "U" -> y++
+    "D" -> y--
+    "L" -> x--
+    else /*RIGHT*/ -> x++
+}
 
-        "D" -> {
-            currentV--
-        }
-
-        "L" -> {
-            currentH--
-        }
-
-        else /*RIGHT*/ -> {
-            currentH++
-        }
-    }
-
-
-private fun KnotPosition.tailFollow(head: KnotPosition, visitedPositions: HashMap<String, Int>?) {
-    val horizontalDistance = head.currentH - currentH
-    val verticalDistance = head.currentV - currentV
+private fun KnotPosition.tailFollow(head: KnotPosition, visitedPositions: HashMap<String, Int>) {
+    val horizontalDistance = head.x - x
+    val verticalDistance = head.y - y
 
     var moved = true
 
@@ -41,55 +28,43 @@ private fun KnotPosition.tailFollow(head: KnotPosition, visitedPositions: HashMa
     */
 
     if (horizontalDistance == 0 && verticalDistance == 2) { // 0 - up
-        currentV++
-    } else if (horizontalDistance == 1 && verticalDistance == 2) { // 1
-        currentV++
-        currentH++
-    } else if (horizontalDistance == 2 && verticalDistance == 2) { // 2
-        currentV++
-        currentH++
-    } else if (horizontalDistance == 2 && verticalDistance == 1) { // 3
-        currentH++
-        currentV++
+        y++
+    } else if (horizontalDistance == 1 && verticalDistance == 2 || // 1 diagonal
+        horizontalDistance == 2 && verticalDistance == 2 || // 2 diagonal
+        horizontalDistance == 2 && verticalDistance == 1 // 3 diagonal
+    ) {
+        x++
+        y++
     } else if (horizontalDistance == 2 && verticalDistance == 0) { // 4 - right
-        currentH++
-    } else if (horizontalDistance == 2 && verticalDistance == -1) { // 5
-        currentH++
-        currentV--
-    } else if (horizontalDistance == 2 && verticalDistance == -2) { // 6
-        currentH++
-        currentV--
-    } else if (horizontalDistance == 1 && verticalDistance == -2) { // 7
-        currentH++
-        currentV--
+        x++
+    } else if (horizontalDistance == 2 && verticalDistance == -1 || // 5 diagonal
+        horizontalDistance == 2 && verticalDistance == -2 || // 6 diagonal
+        horizontalDistance == 1 && verticalDistance == -2 // 7 diagonal
+    ) {
+        x++
+        y--
     } else if (horizontalDistance == 0 && verticalDistance == -2) { // 8 - down
-        currentV--
-    } else if (horizontalDistance == -1 && verticalDistance == -2) { // 9
-        currentV--
-        currentH--
-    } else if (horizontalDistance == -2 && verticalDistance == -2) { // A
-        currentV--
-        currentH--
-    } else if (horizontalDistance == -2 && verticalDistance == -1) { // B
-        currentH--
-        currentV--
+        y--
+    } else if (horizontalDistance == -1 && verticalDistance == -2 || // 9 diagonal
+        horizontalDistance == -2 && verticalDistance == -2 || // A diagonal
+        horizontalDistance == -2 && verticalDistance == -1 // B diagonal
+    ) {
+        x--
+        y--
     } else if (horizontalDistance == -2 && verticalDistance == 0) { // C - left
-        currentH--
-    } else if (horizontalDistance == -2 && verticalDistance == 1) { // D
-        currentH--
-        currentV++
-    } else if (horizontalDistance == -2 && verticalDistance == 2) { // E
-        currentH--
-        currentV++
-    } else if (horizontalDistance == -1 && verticalDistance == 2) { // F
-        currentH--
-        currentV++
+        x--
+    } else if (horizontalDistance == -2 && verticalDistance == 1 || // D diagonal
+        horizontalDistance == -2 && verticalDistance == 2 || // E diagonal
+        horizontalDistance == -1 && verticalDistance == 2  // F diagonal
+    ) {
+        x--
+        y++
     } else {
         moved = false
     }
 
-    if (visitedPositions != null && moved) {
-        val position = "$currentH,$currentV"
+    if (this.name == "T" && moved) {
+        val position = "$x,$y"
         val currentPosition = visitedPositions[position]
         if (currentPosition == null) {
             visitedPositions[position] = 1
@@ -100,7 +75,7 @@ private fun KnotPosition.tailFollow(head: KnotPosition, visitedPositions: HashMa
 }
 
 fun printMap(
-    headTailPosition: MutableList<KnotPosition>,
+    headTailPosition: List<KnotPosition>,
     xStart: Int = 0,
     xEnd: Int = 5,
     yStart: Int = 0,
@@ -109,11 +84,11 @@ fun printMap(
     val totalX = xEnd - xStart
     for (countX in xStart..xEnd) {
         headTailPosition
-            .filter { it.currentV == totalX - countX }
-            .sortedBy { it.currentH }
+            .filter { it.y == totalX - countX }
+            .sortedBy { it.x }
             .also { list ->
                 for (countY in yStart..yEnd) {
-                    val item = list.firstOrNull { it.currentH == countY }
+                    val item = list.firstOrNull { it.x == countY }
                     print(item?.name ?: ".")
                 }
                 println()
@@ -122,50 +97,48 @@ fun printMap(
     println()
 }
 
+fun createRope(size: Int): List<KnotPosition> = mutableListOf<KnotPosition>()
+    .apply {
+        add(KnotPosition("H"))
+        repeat(size - 2) { index ->
+            add(KnotPosition("${index + 1}"))
+        }
+        add(KnotPosition("T"))
+    }
+
 fun main() {
 
     fun part1(input: List<String>): Int {
-        val headTailPosition = mutableListOf(KnotPosition("H"), KnotPosition("T"))
+        val rope = createRope(2)
         val visitedPositions: HashMap<String, Int> = hashMapOf("0,0" to 1)
 
         input.forEach { command ->
-            println("== $command ==")
             val (direction, count) = command.split(" ")
             repeat(count.toInt()) {
-                headTailPosition.first().move(direction)
-                headTailPosition.last().tailFollow(headTailPosition.first(), visitedPositions)
-                printMap(headTailPosition)
+                rope.first().move(direction)
+                rope.last().tailFollow(rope.first(), visitedPositions)
+                printMap(rope)
             }
         }
         return visitedPositions.count()
     }
 
     fun part2(input: List<String>): Int {
-        val headTailPosition = mutableListOf<KnotPosition>().apply {
-            add(KnotPosition("H"))
-            repeat(9) { index ->
-                add(KnotPosition("${index + 1}"))
-            }
-        }
-
+        val rope = createRope(10)
         val visitedPositions: HashMap<String, Int> = hashMapOf("0,0" to 1)
 
         input.forEach { command ->
-            println("== $command ==")
             val (direction, count) = command.split(" ")
             repeat(count.toInt()) {
-                headTailPosition.first().move(direction)
-                headTailPosition.subList(1, headTailPosition.size).forEachIndexed { index, item ->
-                    val newIndex = index + 1
-                    println("moving tail $newIndex")
-                    if (newIndex == headTailPosition.size - 1) {// last item
-                        item.tailFollow(headTailPosition[newIndex - 1], visitedPositions)
-                    } else {
-                        item.tailFollow(headTailPosition[newIndex - 1], null)
+                rope.first().move(direction)
+                rope
+                    .subList(1, rope.size)
+                    .forEachIndexed { index, item ->
+                        val newIndex = index + 1
+                        item.tailFollow(rope[newIndex - 1], visitedPositions)
                     }
-                }
-//                printMap(headTailPosition)
-                printMap(headTailPosition, -5, 16, -11, 15)
+                printMap(rope)
+//                printMap(headTailPosition, -5, 16, -11, 15)
             }
         }
         return visitedPositions.count()
@@ -179,7 +152,6 @@ fun main() {
     check(part2("day09_test2".readFileAsLines()) == 36)
 
     "day09".readFileAsLines().let { input ->
-        println("\n\n=== Starting actual")
         println("part1: ${part1(input)}")
         println("part2: ${part2(input)}")
     }
